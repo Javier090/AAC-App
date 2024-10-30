0546a2b2b62fd637fad4187ad9017f13115c4072
Javier090 <javiergarc161@gmail.com>
2024-10-30 01:18:10 -0400

    Added aditional comments

--- aac-jazz/src/SentenceBuilder.js
+++ aac-jazz/src/SentenceBuilder.js
@@ -4,12 +4,12 @@ import './CommunicationBoard.css';
 
 const SentenceBuilder = ({selectedCards, onCardRemove, onClearAll}) => {
     // Create a sentence by joining the text from the selected cards
-    const sentence = selectedCards.map(card => card.text).join(' ');
-    return (
+    const sentence = selectedCards.map(card => card.text).join(' '); // Join the text of the selected cards with in sentence builder
+    return ( // Render the sentence builder component
       <div className="sentence-container">
         <h2 className="sentence-label">Sentence:</h2>
         <div className="sentence-output">
-          {sentence.length > 0 ? sentence : 'Start selecting words to form a sentence.'}
+          {sentence.length > 0 ? sentence : 'Start selecting words to form a sentence.'} {/* Conditional rendering for sentence depending on sentence lenght */}
         </div>
         <div className="remove-buttons">
           {selectedCards.map(card => (
@@ -22,8 +22,8 @@ const SentenceBuilder = ({selectedCards, onCardRemove, onClearAll}) => {
             </button>
           ))}
         </div>
-        <button onClick={onClearAll} className="clear-all-button">
-          {selectedCards.length > 0 ? 'Clear All' : 'Start Selecting'}
+        <button onClick={onClearAll} className="clear-all-button"> {/* Clear all button */}
+          {selectedCards.length > 0 ? 'Clear All' : 'Start Selecting'}  {/* Conditional rendering for clear all depending on sentence lenght */}
         </button>
       </div>
     );

658dc82478ed9cfcd6ebcc327af746b19e32f7e7
Abernardeau <98547150+Abernardeau@users.noreply.github.com>
2024-11-10 15:58:56 -0500

    Update config.js

--- aac-jazz/server/db/config.js
+++ aac-jazz/server/db/config.js
@@ -1,15 +1,13 @@
-const mysql = require('mysql2/promise'); // Import the MySQL library 
+const mysql = require('mysql2/promise'); // Import the MySQL library
 require('dotenv').config();
 
-// Create a connection pool for mySQL database with environment variables
 const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST,    // Use the public IP address of your Cloud SQL instance
+  user: process.env.DB_USER,    // Use the database username (typically 'root')
+  password: process.env.DB_PASSWORD, // Use the database password
+  database: process.env.DB_NAME // Your database name
 });
 
-// Connection conditional 
 connection.getConnection()
   .then(conn => {
     console.log('Connected to the MySQL database');
@@ -20,4 +18,3 @@ connection.getConnection()
   });
 
 module.exports = connection;
-
